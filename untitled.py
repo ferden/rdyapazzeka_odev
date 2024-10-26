3f366b6ac814b3b341864e256f3dfc16e26ddc73
FikretErden <ferden@gmail.com>
2024-10-26 18:15:24 +0300

    Güncellendi: Ödev dosyası eklendi.

--- untitled.py
+++
@@ -1,5 +0,0 @@
-# RD Ödev 2 - Fikret Erden
-Bu notebook, yolov8m modelinin `Ultralytics YOLOv8` kütüphanesi ile fotoğraf ve video verileri üzerinde nesne tespiti yapmak için kullanılmasını içermektedir.
-
-## Veri Detayları
-Bu çalışmada, fotoğraf ve video dosyaları kullanılmıştır. Fotoğraflar `D:/yzeka/vscodefiles/fotograf` dizininde, videolar ise `D:/yzeka/vscodefiles/video` dizininde yer almaktadır.
